c70c98e25533b9b73421830609f9d0426292d1ec
Emmanuel Zidafamor <emmanuel@zidafamor.com>
2017-10-23 19:52:19 +0000

    Update to JavaScript Code, adding a timer function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var randomNumber = Math.floor(Math.random() * 100) + 1;
+/*  
+ // Author: Emmanuel Zidafamor
+ // http://github.com/e-zidaf
+  
+ // Feel free to copy, use or modify but 
+ // kindly add a linkback to the original author.
+*/
 
+var randomNumber = Math.floor(Math.random() * 100) + 1;
 var guesses = document.querySelector('.guesses');
 var lastResult = document.querySelector('.lastResult');
 var lowOrHi = document.querySelector('.lowOrHi');
-
 var guessSubmit = document.querySelector('.guessSubmit');
 var guessField = document.querySelector('.guessField');
-
 var guessCount = 1;
 var resetButton;
 
@@ -18,13 +23,15 @@ function checkGuess() {
   guesses.textContent += userGuess + ' ';
 
   if (userGuess === randomNumber) {
-    lastResult.textContent = 'Ahaa!!! Congratulations mate. That was the correct answer!'
+    lastResult.textContent = ("Aha!!! The random number is " + randomNumber + "." + " Congratulations mate!");
     lastResult.style.backgroundColor = 'green';
     lowOrHi.textContent = '';
     setGameOver();
   } else if (guessCount === 10) {
     lastResult.textContent = '!!!GAME OVER!!!';
     setGameOver();
+    console.log('Timer stopped!');
+    throw 'Timer stopped';
   } else {
     lastResult.textContent = 'Wrong!';
     lastResult.style.backgroundColor = 'red';
@@ -49,7 +56,55 @@ function setGameOver() {
   resetButton.textContent = 'Start new game';
   document.body.appendChild(resetButton);
   resetButton.addEventListener('click', resetGame);
+  resetButton.addEventListener('click', refreshPage);
 }
+
+
+var interval;
+var minutes = 1;
+var seconds = 10;
+window.onload = function() {
+  countdown('countdown');
+}
+
+function countdown(element) {
+  interval = setInterval(function() {
+    var el = document.getElementById(element);
+
+    if (minutes >= 1) {
+      el.style.backgroundColor = 'white'
+    } else if (seconds > 30) {
+      el.style.backgroundColor = 'yellow'
+    } else {
+      el.style.backgroundColor = 'red'
+    }
+
+    if (seconds == 0) {
+      if (minutes == 0) {
+        el.innerHTML = "Game over!!!";
+        clearInterval(interval);
+        setGameOver();
+        return;
+      } else {
+        minutes--;
+        seconds = 60;
+      }
+    }
+    if (minutes > 0) {
+      var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
+    } else {
+      var minute_text = '';
+    }
+    var second_text = seconds > 1 ? 'seconds' : 'second';
+    el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + ' remaining';
+    seconds--;
+  }, 1000);
+}
+
+function refreshPage() {
+  window.location.reload();
+}
+
 
 function resetGame() {
   guessCount = 1;
